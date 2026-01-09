4fe7a22ec45165ac08389bc88cbb7dfef8ba18e8
Yassr Atti <cloudec31@gmail.com>
2026-01-10 00:17:03 +0100

    make it or die text

--- app/footer.tsx
+++ app/footer.tsx
@@ -73,7 +73,7 @@ export function Footer() {
         <a href="https://github.com/ibelick/nim" target="_blank">
           <TextLoop className="text-xs text-zinc-500">
             <span>© 2024 Yassr </span>
-            <span>Built with Motion-Primitives.</span>
+            <span>Make it or die </span>
           </TextLoop>
         </a>
         <div className="text-xs text-zinc-400">
